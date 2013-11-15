4b60ed678ea6508a454bad64c80f53e0aafc5662
Kunle Adedayo <adedayokunle@gmail.com>
2013-11-15 11:04:29 -0500

    Create UserHospital model

---
+++ app/models/UserHospital.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of UserHospital
+ *
+ * @author kayfun
+ */
+class UserHospital extends Illuminate\Database\Eloquent\Model {
+
+    protected $table = 'user_hospital';
+
+}
